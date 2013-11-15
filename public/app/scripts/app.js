2004c725edbc25c8bc22befbf0dfc4f1d9cb492a
Thiago Felix <felixthi@gmail.com>
2013-11-14 22:44:23 -0200

    First commit front-end

---
+++ public/app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('pacApp', [
+  'ngCookies',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ public/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pacApp')
+
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
