ba66bcf190f6e3acb87b74b53dbc941239fb60da
chiconato <cachiconato@gmail.com>
2013-11-15 16:25:10 -0200

    Now filtering by subeixo

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -2,9 +2,9 @@ class Transport
   include Mongoid::Document
 
   def self.investiments
-    Venture.collection.aggregate({ 
-      '$match' => {'idn_estagio.estagio' => { '$in' => ['Em obras', 'Em execução', 'Concluído', 'Em operação'] }, 'idn_digs.Subeixo' => 'Transportes'},
-      '$group' => { '_id' => {'subeixo' => '$idn_digs.Subeixo', 'data_balanco' => '$dat_ciclo'},
+    Venture.collection.aggregate(
+      { '$match' => {'idn_estagio.estagio' => { '$in' => ['Em obras', 'Em execução', 'Concluído', 'Em operação'] }, 'idn_digs.Subeixo' => 'Transportes'} },
+      { '$group' => { '_id' => {'subeixo' => '$idn_digs.Subeixo', 'data_balanco' => '$dat_ciclo'},
       'valor_total' => { '$sum'  => '$investimento_total' }, 
       'valor_total2' => {'$sum' => '$val_2011_2014'} } } )  
   end
