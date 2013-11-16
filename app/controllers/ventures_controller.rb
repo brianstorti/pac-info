6a91db84ba6e128a9bd9bcbdb5fa7a7621d94220
Brian Thomas Storti <btstorti@gmail.com>
2013-11-16 18:14:50 -0200

    accept firendly names in the routes

--- app/controllers/ventures_controller.rb
+++ app/controllers/ventures_controller.rb
@@ -1,9 +1,5 @@
 class VenturesController < ApplicationController
-  before_filter :store_cache
-
-  def store_cache
-    expires_in 1.month, public: true
-  end
+  before_filter :store_cache, :get_category_name
 
   def index
     render json: Venture.new(params[:category]).investiments
@@ -19,5 +15,21 @@ class VenturesController < ApplicationController
 
   def by_region
     render json: Venture.new(params[:category]).by_region(params[:region])
+  end
+
+  def store_cache
+    expires_in 1.month, public: true
+  end
+
+  def get_category_name
+    params[:category] = case params[:category]
+                        when "transportes" then "Transportes"
+                        when "comunidade-cidada" then "Comunidade Cidadã"
+                        when "habitacao" then "Habitação"
+                        when "energia" then "Energia"
+                        when "agua-e-luz-para-todos" then "Água e Luz para todos"
+                        when "cidade-melhor" then "Cidade Melhor"
+                        else params[:category]
+                        end
   end
 end
