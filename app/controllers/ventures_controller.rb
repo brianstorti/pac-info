ba988e307baf77e4be9ae74c651eefa46f0ad193
Brian Thomas Storti <btstorti@gmail.com>
2013-11-16 17:28:59 -0200

    use instance methods

--- app/controllers/ventures_controller.rb
+++ app/controllers/ventures_controller.rb
@@ -1,17 +1,23 @@
 class VenturesController < ApplicationController
+  before_filter :store_cache
+
+  def store_cache
+    expires_in 1.month, public: true
+  end
+
   def index
-    render json: Venture.investiments_by(params[:category])
+    render json: Venture.new(params[:category]).investiments
   end
 
   def by_type
-    render json: Venture.investiments_by_type(params[:category])
+    render json: Venture.new(params[:category]).by_type
   end
 
   def by_status
-    render json: Venture.investiments_by_status(params[:category])
+    render json: Venture.new(params[:category]).by_status
   end
 
   def by_region
-    render json: Venture.investiments_by_region(params[:category], params[:region])
+    render json: Venture.new(params[:category]).by_region(params[:region])
   end
 end
--- app/models/venture.rb
+++ app/models/venture.rb
@@ -1,38 +1,40 @@
 class Venture
   include Mongoid::Document
 
-  class << self
-    def investiments_by(category)
-      collection.aggregate(
-        { '$match' => { 'idn_estagio.estagio' => { '$in' => ['Em obras', 'Em execução', 'Concluído', 'Em operação'] },
-                        'idn_digs.Subeixo' => category }},
-                        { '$group' => { '_id' => { 'subeixo' => '$idn_digs.Subeixo', 'data_balanco' => '$dat_ciclo' },
-                                        'investimento_total' => { '$sum'  => '$investimento_total' },
-                                        'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
-    end
+  def initialize(category)
+    @category = category
+  end
 
-    def investiments_by_type(category)
-      collection.aggregate(
-        { '$match' => { 'idn_digs.Subeixo' => category }},
-        { '$group' => { '_id' => '$idn_digs.Tipo',
-                        'investimento_total' => { '$sum'  => '$investimento_total' },
-                        'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
-    end
+  def investiments
+    collection.aggregate(
+      { '$match' => { 'idn_estagio.estagio' => { '$in' => ['Em obras', 'Em execução', 'Concluído', 'Em operação'] },
+                      'idn_digs.Subeixo' => @category }},
+                      { '$group' => { '_id' => { 'subeixo' => '$idn_digs.Subeixo', 'data_balanco' => '$dat_ciclo' },
+                                      'investimento_total' => { '$sum'  => '$investimento_total' },
+                                      'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
+  end
 
-    def investiments_by_status(category)
-      collection.aggregate(
-        { '$match' => { 'idn_digs.Subeixo' => category }},
-        { '$group' => { '_id' => '$idn_estagio.estagio',
-                        'total' => { '$sum' => 1 }}})
-    end
+  def by_type
+    collection.aggregate(
+      { '$match' => { 'idn_digs.Subeixo' => @category }},
+      { '$group' => { '_id' => '$idn_digs.Tipo',
+                      'investimento_total' => { '$sum'  => '$investimento_total' },
+                      'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
+  end
 
-    def investiments_by_region(category, region)
-      collection.aggregate(
-        { '$match' => { 'idn_digs.Subeixo' => category,
-                        'sig_uf' => { '$in' => Region.states_in(region) }}},
-        { '$group' => { '_id' => '$sig_uf',
-                        'investimento_total' => { '$sum' => '$investimento_total' },
-                        'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
-    end
+  def by_status
+    collection.aggregate(
+      { '$match' => { 'idn_digs.Subeixo' => @category }},
+      { '$group' => { '_id' => '$idn_estagio.estagio',
+                      'total' => { '$sum' => 1 }}})
+  end
+
+  def by_region(region)
+    collection.aggregate(
+      { '$match' => { 'idn_digs.Subeixo' => @category,
+                      'sig_uf' => { '$in' => Region.states_in(region) }}},
+      { '$group' => { '_id' => '$sig_uf',
+                      'investimento_total' => { '$sum' => '$investimento_total' },
+                      'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
   end
 end
