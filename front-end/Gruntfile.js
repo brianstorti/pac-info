c899d275b712d83712559e2dd6f86ff670b63020
chiconato <cachiconato@gmail.com>
2013-11-16 19:27:10 -0200

    copying assets to public folder

--- front-end/Gruntfile.js
+++ front-end/Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
     yeoman: {
       // configurable paths
       app: require('./bower.json').appPath || 'app',
-      dist: 'dist'
+      dist: '../public'
     },
     watch: {
       coffee: {
