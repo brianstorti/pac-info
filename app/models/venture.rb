e77836b124b263213a05a751f3c1abf2facb956c
Brian Thomas Storti <btstorti@gmail.com>
2013-11-16 17:45:05 -0200

    add label with whe formatted currency

--- app/models/venture.rb
+++ app/models/venture.rb
@@ -6,20 +6,22 @@ class Venture
   end
 
   def investiments
-    collection.aggregate(
+    result = collection.aggregate(
       { '$match' => { 'idn_estagio.estagio' => { '$in' => ['Em obras', 'Em execução', 'Concluído', 'Em operação'] },
                       'idn_digs.Subeixo' => @category }},
                       { '$group' => { '_id' => { 'subeixo' => '$idn_digs.Subeixo', 'data_balanco' => '$dat_ciclo' },
                                       'investimento_total' => { '$sum'  => '$investimento_total' },
                                       'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
+    format_currency(result)
   end
 
   def by_type
-    collection.aggregate(
+    result = collection.aggregate(
       { '$match' => { 'idn_digs.Subeixo' => @category }},
       { '$group' => { '_id' => '$idn_digs.Tipo',
                       'investimento_total' => { '$sum'  => '$investimento_total' },
                       'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
+    format_currency(result)
   end
 
   def by_status
@@ -30,11 +32,24 @@ class Venture
   end
 
   def by_region(region)
-    collection.aggregate(
+    result = collection.aggregate(
       { '$match' => { 'idn_digs.Subeixo' => @category,
                       'sig_uf' => { '$in' => Region.states_in(region) }}},
       { '$group' => { '_id' => '$sig_uf',
                       'investimento_total' => { '$sum' => '$investimento_total' },
                       'val_2011_2014' => { '$sum' => '$val_2011_2014' }}})
+    format_currency(result)
   end
+
+
+  def format_currency(results)
+    formatted_results = []
+    results.each do |result|
+      value = result["investimento_total"] + result ["val_2011_2014"]
+      formatted_results << result.merge(label: CurrencyFormatter.new(value).formatted)
+    end
+
+    formatted_results
+  end
+  private :format_currency
 end
