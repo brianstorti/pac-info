2da26c351ac635046a1ed85c1ecad60447604189
Thiago Felix <felixthi@gmail.com>
2013-11-16 18:51:36 -0200

    Per Region chart

---
+++ public/app/scripts/charts/por-regiao.js
@@ -0,0 +1,122 @@
+'use strict';
+
+angular.module('pacApp')
+	.factory('porRegiaoSpec', ['chartHeight', 'measureElement', function(chartHeight, measureElement){
+		return function(element, data, opts) {
+			var width = measureElement(element).width;
+			return {
+				'width': width,
+				'height': chartHeight,
+				'padding': { 'top': 10, 'left': 0, 'bottom': 30, 'right': 0 },
+				'data': [{'name': 'table'}],
+				'scales': [
+					{
+						'name': 'x',
+						'type': 'ordinal',
+						'range': 'width',
+						'domain': { 'data': 'table', 'field': 'data._id' }
+					},
+					{
+						'name': 'y',
+						'range': 'height',
+						'nice': true,
+						'domain': { 'data': 'table', 'field': 'data.val_2011_2014' },
+						'domainMax': opts.domainMax || undefined
+					}
+				],
+				'axes': [
+					{
+						'type': 'x',
+						'scale': 'x',
+						'properties': {
+							'axis': { 'strokeWidth': { 'value': 0 } },
+							'majorTicks': { 'strokeWidth': {'value': 0} },
+							'labels': {
+								'fill': { 'value': '#FFF' },
+								'angle': { 'value': 0 },
+								'fontSize': { 'value': 12 },
+								'fontWeight': { 'value': '200' },
+								'align': { 'value': 'center'}
+							}
+						}
+					}
+				],
+				'marks': [
+					{
+						'key': 'data._id',
+						'type': 'rect',
+						'from': { 'data': 'table' },
+						'properties': {
+							'enter': {
+								'x': {'scale': 'x', 'field': 'data._id', 'offset': 15},
+								'y': { 'scale': 'y', 'field': 'data.val_2011_2014', 'offset': 5 },
+								'y2': { 'scale': 'y', 'value': 0 },
+								'fill': {'value': '#fff'},
+								'width': {'scale': 'x', 'band': true, 'offset': -30},
+							},
+							'update': {
+								'x': {'scale': 'x', 'field': 'data._id', 'offset': 15},
+								'y': { 'scale': 'y', 'field': 'data.val_2011_2014', 'offset': 5 },
+								'y2': { 'scale': 'y', 'value': 0 },
+								'width': {'scale': 'x', 'band': true, 'offset': -30}
+							}
+						}
+					},
+					{
+						'type': 'text',
+						'from': { 'data': 'table' },
+						'properties': {
+							'enter': {
+								'x': {'scale': 'x', 'field': 'data._id'},
+								'y': {'scale': 'y', 'field': 'data.val_2011_2014'},
+								'fill': {'value': '#fff'},
+								'text': {'field': 'data.label'},
+								'fontSize': { 'value': 14 }
+							},
+							'update': {
+								'x': {'scale': 'x', 'field': 'data._id'},
+								'y': {'scale': 'y', 'field': 'data.val_2011_2014'},
+								'text': {'field': 'data.label'},
+							}
+						}
+					}
+				]
+			};
+		};
+	}])
+	.service('porRegiaoChart',[
+		'API_URL',
+		'$http',
+		'porRegiaoSpec',
+		'emptyDataChart',
+		function(API_URL, $http, evolucaoSpec, emptyDataChart){
+			var that = this;
+
+			this.spec = evolucaoSpec;
+			this.data = emptyDataChart;
+
+			this.clearResponseElement = function(responseElement){
+				var clearObj = angular.copy(responseElement);
+				clearObj['val_2011_2014'] = '';
+				return clearObj;
+			};
+
+			this.transformResponse = function(investimentosPorTipo){
+				var emptyData = [];
+
+				for (var i = 0; i < investimentosPorTipo.length; i++) {
+					emptyData.push( that.clearResponseElement(investimentosPorTipo[i]) );
+				}
+
+				return {
+					full: { table: investimentosPorTipo },
+					empty: { table: emptyData }
+				};
+			};
+
+			this.carregarCategoria = function(categoria, regiao){
+				var url = API_URL + [categoria, 'by_regiao', regiao].join('/');
+				$http.get(url).success(function(data){ that.data = that.transformResponse(data); });
+			};
+		}
+	]);
--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -3,150 +3,20 @@
 angular.module('pacApp')
 	.controller('MainCtrl',[
 		'$scope',
-		'estagioSpec',
-		'distribuicaoSpec',
+		'porRegiaoChart',
 		'estagioChart',
 		'evolucaoChart',
 		'distribuicaoChart',
 		'$location',
-		function ($scope, estagioSpec, distribuicaoSpec, estagioChart, evolucaoChart, distribuicaoChart, $location) {
+		function ($scope, porRegiaoChart, estagioChart, evolucaoChart, distribuicaoChart, $location) {
 
 			var regioes = [
-				{
-					name: 'Norte',
-					data: {
-						full : {
-							table: [
-								{ 'estado':'', 'x': 'Portos', 'y': 40 },
-								{ 'estado':'', 'x': 'Rodovias', 'y': 100 },
-								{ 'estado':'', 'x': 'Ferrovias', 'y': 80 },
-								{ 'estado':'', 'x': 'Hidrovias', 'y': 60 },
-								{ 'estado':'', 'x': 'Aeroportos', 'y': 15 }
-							]
-						},
-						empty : {
-							table: [
-								{ 'estado':'', 'x': 'Portos', 'y': '' },
-								{ 'estado':'', 'x': 'Rodovias', 'y': '' },
-								{ 'estado':'', 'x': 'Ferrovias', 'y': '' },
-								{ 'estado':'', 'x': 'Hidrovias', 'y': '' },
-								{ 'estado':'', 'x': 'Aeroportos', 'y': '' }
-							]
-						}
-					}
-				},
-				{
-					name: 'Nordeste',
-					data: {
-						full : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': 10 },
-								{ 'estado': '', 'x': 'Rodovias', 'y': 10 },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': 8 },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': 20 },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': 15 }
-							]
-						},
-						empty : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': '' },
-								{ 'estado': '', 'x': 'Rodovias', 'y': '' },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': '' }
-							]
-						}
-					}
-				},
-				{
-					name: 'Centro-Oeste',
-					data: {
-						full : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': 40 },
-								{ 'estado': '', 'x': 'Rodovias', 'y': 100 },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': 80 },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': 60 },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': 15 }
-							]
-						},
-						empty : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': '' },
-								{ 'estado': '', 'x': 'Rodovias', 'y': '' },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': '' }
-							]
-						}
-					}
-				},
-				{
-					name: 'Sudeste',
-					data: {
-						full : {
-							table: [
-								{ 'estado': 'SP', 'x': 'Portos', 'y': 400 },
-								{ 'estado': 'SP', 'x': 'Rodovias', 'y': 1000 },
-								{ 'estado': 'SP', 'x': 'Ferrovias', 'y': 800 },
-								{ 'estado': 'SP', 'x': 'Hidrovias', 'y': 600 },
-								{ 'estado': 'SP', 'x': 'Aeroportos', 'y': 150 },
-
-								{ 'estado': 'RJ', 'x': 'Portos', 'y': 140 },
-								{ 'estado': 'RJ', 'x': 'Rodovias', 'y': 1100 },
-								{ 'estado': 'RJ', 'x': 'Ferrovias', 'y': 180 },
-								{ 'estado': 'RJ', 'x': 'Hidrovias', 'y': 160 },
-								{ 'estado': 'RJ', 'x': 'Aeroportos', 'y': 115 },
-
-								{ 'estado': 'MG', 'x': 'Portos', 'y': 30 },
-								{ 'estado': 'MG', 'x': 'Rodovias', 'y': 60 },
-								{ 'estado': 'MG', 'x': 'Ferrovias', 'y': 90 },
-								{ 'estado': 'MG', 'x': 'Hidrovias', 'y': 100 },
-								{ 'estado': 'MG', 'x': 'Aeroportos', 'y': 50 },
-
-								{ 'estado': 'ES', 'x': 'Portos', 'y': 20 },
-								{ 'estado': 'ES', 'x': 'Rodovias', 'y': 10 },
-								{ 'estado': 'ES', 'x': 'Ferrovias', 'y': 30 },
-								{ 'estado': 'ES', 'x': 'Hidrovias', 'y': 10 },
-								{ 'estado': 'ES', 'x': 'Aeroportos', 'y': 5 }
-							]
-						},
-						empty : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': '' },
-								{ 'estado': '', 'x': 'Rodovias', 'y': '' },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': '' }
-							]
-						}
-					}
-				},
-				{
-					name: 'Sul',
-					data: {
-						full : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': 40 },
-								{ 'estado': '', 'x': 'Rodovias', 'y': 100 },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': 80 },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': 60 },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': 15 }
-							]
-						},
-						empty : {
-							table: [
-								{ 'estado': '', 'x': 'Portos', 'y': '' },
-								{ 'estado': '', 'x': 'Rodovias', 'y': '' },
-								{ 'estado': '', 'x': 'Ferrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Hidrovias', 'y': '' },
-								{ 'estado': '', 'x': 'Aeroportos', 'y': '' }
-							]
-						}
-					}
-				}
+				{ name: 'Norte' },
+				{ name: 'Nordeste' },
+				{ name: 'Centro-Oeste' },
+				{ name: 'Sudeste' },
+				{ name: 'Sul' }
 			];
-
 
 			$scope.evolucao = evolucaoChart;
 
@@ -154,50 +24,49 @@ angular.module('pacApp')
 
 			$scope.estagios = estagioChart;
 
+			$scope.porRegiao = porRegiaoChart;
+
 			$scope.slides = {
-					list: regioes,
-					current: regioes[0],
-					next: function(){
-						var list = $scope.slides.list,
-								current = $scope.slides.current,
-								idx = list.indexOf(current) + 1;
+				list: regioes,
+				current: regioes[0],
+				next: function(){
+					$scope.slides.move('right');
+				},
+				prev: function(){
+					$scope.slides.move('left');
+				},
+				move: function(type) {
+					var list = $scope.slides.list,
+							current = $scope.slides.current,
+							idxRight = list.indexOf(current) + 1,
+							idxLeft = (list.indexOf(current) || list.length) - 1,
+							idx = (type==='right')?idxRight:idxLeft;
 
-						$scope.slides.current = list[idx % list.length];
-						$scope.porRegiao.data = $scope.slides.current.data;
-					},
-					prev: function(){
-						var list = $scope.slides.list,
-								current = $scope.slides.current,
-								idx = (list.indexOf(current) || list.length) - 1;
+					$scope.slides.current = list[idx % list.length];
 
-						$scope.slides.current = list[idx % list.length];
-						$scope.porRegiao.data = $scope.slides.current.data;
-					}
-			}
-
-			$scope.porRegiao = {
-				opts: { domainMax: 100 },
-				spec: distribuicaoSpec,
-				data: $scope.slides.current.data
+					$scope.porRegiao.carregarCategoria($scope.banner.api, $scope.slides.current.name);
+				}
 			};
 
 			$scope.$on('$locationChangeSuccess', function(){
-				var categoriaApi = $location.path().replace('/', ''),
+				var categoriaApi = $location.path(),
 					categoriasMap = {
-						'transportes': 'Transportes',
-						'comunidade-cidada': 'Comunidade Cidadã',
-						'habitacao': 'Habitação',
-						'agua-e-luz-para-todos' : 'Água e Luz para todos',
-						'cidade-melhor': 'Cidade Melhor',
-						'energia': 'Energia',
+						'/transportes': 'Transportes',
+						'/comunidade-cidada': 'Comunidade Cidadã',
+						'/habitacao': 'Habitação',
+						'/agua-e-luz-para-todos' : 'Água e Luz para todos',
+						'/cidade-melhor': 'Cidade Melhor',
+						'/energia': 'Energia',
 					},
 					categoriaNome = categoriasMap[categoriaApi];
 
 				$scope.evolucao.carregarCategoria(categoriaApi);
 				$scope.distribuicao.carregarCategoria(categoriaApi);
 				$scope.estagios.carregarCategoria(categoriaApi);
+				$scope.porRegiao.carregarCategoria(categoriaApi, $scope.slides.current.name);
 
-				$scope.banner = {title: categoriaNome};
+
+				$scope.banner = {title: categoriaNome, api: categoriaApi};
 
 			});
 		}
