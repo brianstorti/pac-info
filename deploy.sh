7e65bdfc84e088eae469582da59af97e1790b459
Thiago Felix <felixthi@gmail.com>
2013-11-17 13:09:31 -0200

    Updating deploy script to update front-end files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+rm -rf public
+cd front-end
+grunt build
+cd ..
+git add public
+git commit -m "Updating version"
 git push origin master
 git push heroku master
 heroku run rails runner -e production Dalli::Client.new.flush
