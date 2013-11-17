8433483efbb055b38ab46febac3119f7d4644b48
Brian Thomas Storti <btstorti@gmail.com>
2013-11-17 01:01:24 -0200

    remove application controller

--- app/controllers/ventures_controller.rb
+++ app/controllers/ventures_controller.rb
@@ -1,35 +1,37 @@
-class VenturesController < ApplicationController
-  before_filter :store_cache, :get_category_name
+class VenturesController < ActionController::API
+  before_filter :store_cache, :get_venture
 
   def index
-    render json: Venture.new(params[:category]).investiments
+    render json: @venture.investiments
   end
 
   def by_type
-    render json: Venture.new(params[:category]).by_type
+    render json: @venture.by_type
   end
 
   def by_status
-    render json: Venture.new(params[:category]).by_status
+    render json: @venture.by_status
   end
 
   def by_region
-    render json: Venture.new(params[:category]).by_region(params[:region])
+    render json: @venture.by_region(params[:region])
   end
 
   def store_cache
     expires_in 1.month, public: true
   end
 
-  def get_category_name
-    params[:category] = case params[:category]
-                        when "transportes" then "Transportes"
-                        when "comunidade-cidada" then "Comunidade Cidadã"
-                        when "habitacao" then "Habitação"
-                        when "energia" then "Energia"
-                        when "agua-e-luz-para-todos" then "Água e Luz para todos"
-                        when "cidade-melhor" then "Cidade Melhor"
-                        else params[:category]
-                        end
+  def get_venture
+    category = case params[:category]
+               when "transportes" then "Transportes"
+               when "comunidade-cidada" then "Comunidade Cidadã"
+               when "habitacao" then "Habitação"
+               when "energia" then "Energia"
+               when "agua-e-luz-para-todos" then "Água e Luz para todos"
+               when "cidade-melhor" then "Cidade Melhor"
+               else params[:category]
+               end
+
+    @venture = Venture.new(category)
   end
 end
