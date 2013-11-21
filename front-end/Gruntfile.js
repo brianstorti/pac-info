47b755fb4cd1fd969cca21075a766e75f2e7880e
Thiago Felix <felixthi@gmail.com>
2013-11-21 10:55:49 -0200

    Less strict htmlmin

--- front-end/Gruntfile.js
+++ front-end/Gruntfile.js
@@ -204,15 +204,15 @@ module.exports = function (grunt) {
 		htmlmin: {
 			dist: {
 				options: {
-					removeCommentsFromCDATA: true,
+					// removeCommentsFromCDATA: true,
 					// https://github.com/yeoman/grunt-usemin/issues/44
 					//collapseWhitespace: true,
-					collapseBooleanAttributes: true,
+					// collapseBooleanAttributes: true,
 					// removeAttributeQuotes: true,
-					removeRedundantAttributes: true,
-					useShortDoctype: true,
+					// removeRedundantAttributes: true,
+					// useShortDoctype: true,
 					// removeEmptyAttributes: true,
-					removeOptionalTags: true
+					// removeOptionalTags: true
 				},
 				files: [{
 					expand: true,
